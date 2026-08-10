38aa020ad2d4e0cb5c5048e6891df0a5021968f1
sort7er <98805454+EdderDev@users.noreply.github.com>
2026-08-11 00:22:55 +0200

    End of section 6

--- client/src/fearures/activities/dashboard/ActivityDashboard.tsx
+++ client/src/fearures/activities/dashboard/ActivityDashboard.tsx
@@ -11,8 +11,6 @@ type Props = {
     openForm: (id: string) => void;
     closeForm: () => void;
     editMode: boolean;
-    submitForm: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
 }
 
 export default function ActivityDashboard({
@@ -22,9 +20,7 @@ export default function ActivityDashboard({
     selectedActivity,
     openForm,
     closeForm,
-    editMode,
-    submitForm,
-    deleteActivity
+    editMode
 }: Props) {
     return (
         <Grid container spacing={3}>
@@ -32,13 +28,12 @@ export default function ActivityDashboard({
                 <ActivityList
                     activities={activities}
                     selectActivity={selectActivity}
-                    deleteActivity={deleteActivity}
                 />
             </Grid>
             <Grid size={5}>
                 {selectedActivity && !editMode &&
                     <ActivityDetails
-                        activity={selectedActivity}
+                        selectedActivity={selectedActivity}
                         cancelSelectActivity={cancelSelectActivity}
                         openForm={openForm}
                     />
@@ -47,7 +42,6 @@ export default function ActivityDashboard({
                     <ActivityForm
                         activity={selectedActivity}
                         closeForm={closeForm}
-                        submitForm={submitForm}
                     />
                 }
             </Grid>
