ab46e274ae08b796c09a90a367210dae07bd434b
sort7er <98805454+EdderDev@users.noreply.github.com>
2026-08-17 18:42:08 +0200

    End of section 11

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import { Box, Container, CssBaseline } from "@mui/material";
 import NavBar from "./NavBar";
 import { Outlet, useLocation } from "react-router";
-import HomePage from "../../fearures/home/HomePage";
+import HomePage from "../../features/home/HomePage";
 
 function App() {
 
--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, Navigate } from "react-router";
 import App from "../layout/App";
-import HomePage from "../../fearures/home/HomePage";
-import ActivityDashboard from "../../fearures/activities/dashboard/ActivityDashboard";
-import ActivityForm from "../../fearures/activities/form/ActivityForm";
-import ActivityDetailsPage from "../../fearures/activities/details/ActivityDetailsPage";
-import Counter from "../../fearures/counter/Counter";
-import TestErrors from "../../fearures/errors/TestErrors";
-import NotFound from "../../fearures/errors/NotFound";
-import ServerError from "../../fearures/errors/ServerError";
+import HomePage from "../../features/home/HomePage";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetailsPage from "../../features/activities/details/ActivityDetailsPage";
+import Counter from "../../features/counter/Counter";
+import TestErrors from "../../features/errors/TestErrors";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
 
 export const router = createBrowserRouter([
