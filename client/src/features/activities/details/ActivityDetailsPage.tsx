333370ef59e1bdec0aa7987ac71d44ec2b9dbd3c
sort7er <edvart.rendum@gmail.com>
2026-08-20 16:57:46 +0200

    End of section 15

--- client/src/features/activities/details/ActivityDetailsPage.tsx
+++ client/src/features/activities/details/ActivityDetailsPage.tsx
@@ -30,7 +30,7 @@ export default function ActivityDetailsPage() {
                 <ActivityDetailsChat />
             </Grid>
             <Grid size={4}>
-                <ActivityDetailsSidebar />
+                <ActivityDetailsSidebar activity={activity} />
             </Grid>
         </Grid>
     )
