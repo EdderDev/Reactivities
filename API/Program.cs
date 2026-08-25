7b4f094beac0d93f7c456669e748c644164a9929
sort7er <98805454+EdderDev@users.noreply.github.com>
2026-08-25 12:45:37 +0200

    End of section 19

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using API.SignalR;
 using Application.Activities.Queries;
 using Application.Activities.Validators;
 using Application.Core;
@@ -27,6 +28,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddSignalR();
 builder.Services.AddMediatR(x =>
 {
     x.LicenseKey = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ikx1Y2t5UGVubnlTb2Z0d2FyZUxpY2Vuc2VLZXkvYmJiMTNhY2I1OTkwNGQ4OWI0Y2IxYzg1ZjA4OGNjZjkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2x1Y2t5cGVubnlzb2Z0d2FyZS5jb20iLCJhdWQiOiJMdWNreVBlbm55U29mdHdhcmUiLCJleHAiOiIxODE3NTEwNDAwIiwiaWF0IjoiMTc4NjAyNDAyOCIsImFjY291bnRfaWQiOiIwMTlmZDc1MzgwMzE3NTY3ODg4Njc2MmEyNGNjMTkwMSIsImN1c3RvbWVyX2lkIjoiMDE5ZmQ3NTM4MDMxNzU2Nzg4ODY3NjJhMjRjYzE5MDEiLCJzdWJfaWQiOiItIiwiZWRpdGlvbiI6IjAiLCJ0eXBlIjoiMiJ9.JR6Cxo3PlM9BFcJ1dH7WbCEfzGvCUg3H8AiazXE7Wh-54pzS8k8A0Fqr5KwRwH4tdCYVv0JGt5C2YPaVK7c5LEkeWx0eiLCzME-H3-6tkUKz1qpK4k6fXSQMPByF721D8V2SaTsdhWZR_ftTD9dLiJOhVo1s9hWeLOGAusLxSSvWjRyR2WaR14IFU1Wa0DDzSe0O9OGt6bPJutLOd7Ch_3MQ8VwzjNOccKuvAV5JHinRmVOMcNy9hgGz3cPiSFweORVaIAVgybMS5btBRXg2Df2CC7HXkKaH8YncW6f1Cb9q0CGHYzf8KwIzEfd5Ahc2bitGyGCKG045U8DtHmLChw";
@@ -70,6 +72,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.MapGroup("api").MapIdentityApi<User>(); //api/login
+app.MapHub<CommentHub>("/comments");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -18,5 +18,6 @@ public class Activity
 
     // Navigation properties
     public ICollection<ActivityAttendee> Attendees { get; set; } = [];
+    public ICollection<Comment> Comments { get; set; } = [];
 
 }
