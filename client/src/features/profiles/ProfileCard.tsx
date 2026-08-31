aa3806a35b20ab005af45bb776c8f4be26c64c65
sort7er <98805454+EdderDev@users.noreply.github.com>
2026-08-31 16:39:21 +0200

    Fix: Profile card image missing

--- client/src/features/profiles/ProfileCard.tsx
+++ client/src/features/profiles/ProfileCard.tsx
@@ -21,7 +21,7 @@ export default function ProfileCard({ profile }: Props) {
             >
                 <CardMedia
                     component="img"
-                    src={profile?.imageUrl || "images/user.png"}
+                    src={profile?.imageUrl || "/images/user.png"}
                     sx={{ width: "100%", zIndex: 50 }}
                     alt={profile.displayName + " image"}
                 />
@@ -29,15 +29,15 @@ export default function ProfileCard({ profile }: Props) {
                     <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
                         <Typography variant="h5">{profile.displayName}</Typography>
                         {profile.bio && (
-                            <Typography variant="body2" sx={{textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>{profile.bio}</Typography>
+                            <Typography variant="body2" sx={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}>{profile.bio}</Typography>
                         )}
                         {profile.following && <Chip size="small" label="Following" color="secondary" variant="outlined" />}
                     </Box>
                 </CardContent>
                 <Divider sx={{ mb: 2 }} />
                 <Box sx={{ display: "flex", alignItems: "center", justifyContent: "start" }}>
-                    <Person/>
-                    <Typography sx={{ml: 1}}>{profile.followersCount} followers</Typography>
+                    <Person />
+                    <Typography sx={{ ml: 1 }}>{profile.followersCount} followers</Typography>
                 </Box>
             </Card>
         </Link>
